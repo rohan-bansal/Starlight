c868b27e1d9695225f7e747a85f409a986f3f794
Rohan Bansal <bansalrohan18@gmail.com>
2019-03-23 13:55:57 -0700

    test #2

--- app/src/main/java/com/rohan/chattranslator/MainActivity.java
+++ app/src/main/java/com/rohan/chattranslator/MainActivity.java
@@ -2,25 +2,34 @@ package com.rohan.chattranslator;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAnalytics mFirebaseAnalytics;
     public static int SIGN_IN_REQUEST_CODE = 10;
+    private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
             displayChatMessages();
         }
 
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                input.setText("");
+            }
+        });
+
     }
 
     @Override
@@ -66,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
                         "Sign in failed.",
                         Toast.LENGTH_LONG)
                         .show();
-
                 finish();
             }
         }
@@ -90,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
                                     Toast.LENGTH_LONG)
                                     .show();
 
-                            // Close activity
                             finish();
                         }
                     });
@@ -100,6 +127,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayChatMessages() {
 
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
+
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                R.layout.message, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 
 
