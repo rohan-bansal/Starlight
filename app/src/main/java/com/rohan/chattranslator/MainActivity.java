13bb4fbaa24188481e05050e8da61a9963fcfdd0
Rohan Bansal <bansalrohan18@gmail.com>
2019-03-23 10:24:28 -0700

    more stuff

--- app/src/main/java/com/rohan/chattranslator/MainActivity.java
+++ app/src/main/java/com/rohan/chattranslator/MainActivity.java
@@ -3,11 +3,25 @@ package com.rohan.chattranslator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
